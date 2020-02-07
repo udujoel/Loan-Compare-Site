5ed9e6d6aef00e9402afae7ccd2ba6e96f3a8405
Joell <amamihechi.udu@decagon.dev>
2020-02-07 21:06:36 +0100

    added HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace LoanCompareSite.Controllers
 {
+
+    [HandleError]
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -15,16 +13,9 @@ namespace LoanCompareSite.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
     }
 }
