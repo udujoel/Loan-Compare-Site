291b0e56291e45f6064e02ff33826c8d186c8c70
Joell <amamihechi.udu@decagon.dev>
2020-02-07 21:11:38 +0100

    Added models

---
+++ Models/LoanDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LoanCompareSite.Models
+{
+    public class LoansDetail
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string package { get; set; }
+        public long minAmount { get; set; }
+        public long maxAmount { get; set; }
+        public int count { get; set; }
+        public DateTime date { get; set; }
+        public double rate { get; set; }
+        public string terms { get; set; }
+        public string website { get; set; }
+        public int duration { get; set; }
+
+
+    }
+
+}
---
+++ Models/LoanTerms.cs
@@ -0,0 +1,14 @@
+﻿namespace LoanCompareSite.Models
+{
+    public class Loanterms
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string package { get; set; }
+        public int count { get; set; }
+        public double rate { get; set; }
+        public string terms { get; set; }
+        public string website { get; set; }
+        public int duration { get; set; }
+    }
+}
