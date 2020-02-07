48aa524526d82a07a382fa7387eb9cdf7c084c57
Joell <amamihechi.udu@decagon.dev>
2020-02-07 21:13:46 +0100

    added viewmodels

---
+++ Models/viewModels/RepaymentDetail.cs
@@ -0,0 +1,12 @@
+﻿namespace LoanCompareSite.Models.viewModels
+{
+    public class RepaymentDetail
+    {
+        public long total { get; set; }
+        public long balance { get; set; }
+        public int monthno { get; set; }
+        public long amountToPay { get; set; }
+        public int payPercent { get; set; }
+
+    }
+}
