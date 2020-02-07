e694a6dcc12fbaaa2be8f167ce882a293dd59ba1
Joell <amamihechi.udu@decagon.dev>
2020-02-07 21:03:19 +0100

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LoanCompareSite.Startup))]
+namespace LoanCompareSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
