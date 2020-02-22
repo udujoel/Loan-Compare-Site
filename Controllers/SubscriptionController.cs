3c22fcc81b7cde2508b914564f17994bb737f440
Joell <amamihechi.udu@decagon.dev>
2020-02-22 09:52:05 +0100

    added payment/subscription page

---
+++ Controllers/SubscriptionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoanCompareSite.Controllers
+{
+    public class SubscriptionController : Controller
+    {
+        // GET: Checkout
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
