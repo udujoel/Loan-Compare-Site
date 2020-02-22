9f355e0a589bf7ab877aa09372f2f1ea00778bca
Joell <amamihechi.udu@decagon.dev>
2020-02-22 10:54:21 +0100

    payment feedback page

---
+++ Controllers/CallbackController.cs
@@ -0,0 +1,29 @@
+﻿using Paystack.Net.SDK.Transactions;
+
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace LoanCompareSite.Controllers
+{
+    public class CallbackController : Controller
+    {
+        [Route("order/callback")]
+        public async Task<ActionResult> Index()
+        {
+            string secretKey = ConfigurationManager.AppSettings["PaystackSecret"];
+            var paystackTransactionAPI = new PaystackTransaction(secretKey);
+            var tranxRef = HttpContext.Request.QueryString["reference"];
+            if (tranxRef != null)
+            {
+                var response = await paystackTransactionAPI.VerifyTransaction(tranxRef);
+                if (response.status)
+                {
+                    return View(response);
+                }
+            }
+
+            return View("PaymentError");
+        }
+    }
+}
