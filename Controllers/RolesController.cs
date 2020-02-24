aa5c47fe6c1708a6be2314306c885cb29ad8e0e8
Joell <amamihechi.udu@decagon.dev>
2020-02-24 02:13:55 +0100

    Roles listing

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -1,5 +1,6 @@
-﻿using LoanCompareSite.Models.EF;
+﻿using LoanCompareSite.Models;
 
+using System.Linq;
 using System.Web.Mvc;
 
 namespace LoanCompareSite.Controllers
@@ -7,19 +8,14 @@ namespace LoanCompareSite.Controllers
 
     public class RolesController : Controller
     {
-        private LoanComparerModel context = new LoanComparerModel();
-        // GET: ManageUserRoles
+        private ApplicationDbContext context = new ApplicationDbContext();
+        // GET:Roles
         public ActionResult Index()
         {
-            // prepopulat roles for the view dropdown
 
-            //            var list = context.Roles.OrderBy(r => r.Name).ToList().Select(rr =>new SelectListItem
-            //            {
-            //               Value = rr.Name.ToString(),Text = rr.Name}).ToList();
-            //            ViewBag.Roles = list;
-            return View();
+            return View(context.Roles.ToList());
         }
-        //
+
         //        public ActionResult ManageUsers()
         //        {
         //            // prepopulat roles for the view dropdown
@@ -28,7 +24,7 @@ namespace LoanCompareSite.Controllers
         //            ViewBag.Roles = list;
         //            return View();
         //        }
-        //
+        //        
         //        [HttpPost]
         //        [ValidateAntiForgeryToken]
         //        public ActionResult RoleAddToUser(string UserName, string RoleName)
@@ -36,13 +32,13 @@ namespace LoanCompareSite.Controllers
         //            ApplicationUser user = context.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
         //            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
         //            manager.AddToRole(user.Id, RoleName);
-        //
+        //        
         //            //ViewBag.ResultMessage = "Role created successfully !";
-        //
+        //        
         //            // prepopulat roles for the view dropdown
         //            var list = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
         //            ViewBag.Roles = list;
-        //
+        //        
         //            return View("ManageUsers");
         //        }
         //        [HttpPost]
@@ -53,14 +49,14 @@ namespace LoanCompareSite.Controllers
         //            {
         //                ApplicationUser user = context.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
         //                var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
-        //
+        //        
         //                ViewBag.RolesForThisUser = manager.GetRoles(user.Id);
-        //
+        //        
         //                // prepopulat roles for the view dropdown
         //                var list = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
         //                ViewBag.Roles = list;
         //            }
-        //
+        //        
         //            return View("ManageUsers");
         //        }
         //        [HttpPost]
@@ -68,9 +64,9 @@ namespace LoanCompareSite.Controllers
         //        public ActionResult DeleteRoleForUser(string UserName, string RoleName)
         //        {
         //            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
-        //
+        //        
         //            ApplicationUser user = context.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
-        //
+        //        
         //            if (manager.IsInRole(user.Id, RoleName))
         //            {
         //                manager.RemoveFromRole(user.Id, RoleName);
@@ -83,7 +79,7 @@ namespace LoanCompareSite.Controllers
         //            // prepopulat roles for the view dropdown
         //            var list = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
         //            ViewBag.Roles = list;
-        //
+        //        
         //            return View("ManageUsers");
         //        }
 
