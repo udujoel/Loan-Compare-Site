99ae7508daa05caf0772758b4ead23a3622a6806
Joell <amamihechi.udu@decagon.dev>
2020-02-24 08:23:26 +0100

    added Admin role enforcing and page

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace LoanCompareSite.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        [Authorize(Roles = "admin")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
