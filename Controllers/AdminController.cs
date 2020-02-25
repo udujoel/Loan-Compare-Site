2fbe7cc1c9915735786b401c3be09e844b6c2481
Joell <amamihechi.udu@decagon.dev>
2020-02-25 12:00:43 +0100

    table formatting

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -9,6 +9,8 @@ namespace LoanCompareSite.Controllers
 {
     public class AdminController : Controller
     {
+
+
         // GET: Admin
         [Authorize(Roles = "admin")]
         public ActionResult Index()
